2e1fa52db36d7523f3b64d598e420d93fdfb5ef9
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2022-06-07 12:23:04 +0200

    feat: add some documentation

--- README.md
+++ README.md
@@ -20,11 +20,52 @@ Run the Composer require command from the Terminal:
 composer require offline-agency/laravel-fatture-in-cloud-v2
 ```
 
+You should publish config file with:
+
+```bash
+php artisan vendor:publish --provider="Offlineagency\LaravelWebex\Providers\LaravelWebexServiceProvider"
+```
+
+Package provide multiple-companies handling. In your config you can provide more companies like that 
+```php
+... 
+
+'companies' => [
+    'default' => [
+        'id' => env('FCV2_DEFAULT_ID', ''),
+        'bearer' => env('FCV2_DEFAULT_BEARER', '')
+    ],
+    'first_company' => [
+        'id' => env('FCV2_FIRST_COMPANY_ID', ''),
+        'bearer' => env('FCV2_FIRST_COMPANY_BEARER', '')
+    ],
+    'second_company' => [
+        'id' => env('FCV2_SECOND_COMPANY_ID', ''),
+        'bearer' => env('FCV2_SECOND_COMPANY_BEARER', '')
+    ]
+]
+```
+
+Then you can specify (or not) a company on class initialization:
+```php
+// take the default
+$issued_documents = new IssuedDocument();
+
+// specify company
+$issued_documents = new IssuedDocument('first_company');
+```
 ## Usage
 
+Each callback accept a number of parameters equals to the sum of the required parameters +1 that is $additional_data
+that accept all optional parameters.
 
 ## Examples
 
+### Issued documents
+```php
+$issued_documents = new IssuedDocument();
+$response = $issued_documents->list($document_type);
+```
 
 ## Testing
 
--- src/LaravelFattureInCloudV2ServiceProvider.php
+++ src/LaravelFattureInCloudV2ServiceProvider.php
@@ -11,36 +11,10 @@ class LaravelFattureInCloudV2ServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-fatture-in-cloud-v2');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-fatture-in-cloud-v2');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/fatture-in-cloud-v2.php' => config_path('fatture-in-cloud-v2.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-fatture-in-cloud-v2'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-fatture-in-cloud-v2'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-fatture-in-cloud-v2'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
