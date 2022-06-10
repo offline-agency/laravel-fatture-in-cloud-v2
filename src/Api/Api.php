5c5b57dc0f747c06bf0707fafb86db77bc8655d0
Nicolas Sanavia <bsn.sanavia.nicolas@gmail.com>
2022-06-10 12:38:48 +0200

    feat: add throttle handling

--- src/Api/Api.php
+++ src/Api/Api.php
@@ -8,22 +8,40 @@ class Api extends LaravelFattureInCloudV2
 {
     protected function get(
         string $url,
-        array $query_parameters = []
-    ): object {
-        $url = $this->baseUrl.$url;
+        array  $query_parameters = []
+    ): object
+    {
+        $complete_url = $this->baseUrl . $url;
 
-        $response = $this->header->get($url, $query_parameters);
+        $response = $this->header->get($complete_url, $query_parameters);
+
+        $response_status = $response->status();
+
+        if ($response_status === 403 || $response_status === 429) {
+            $this->waitThrottle($response_status);
+
+            $this->get($url, $query_parameters);
+        }
 
         return $this->parseResponse($response);
     }
 
     protected function destroy(
         string $url,
-        array $query_parameters = []
-    ): object {
-        $url = $this->baseUrl.$url;
+        array  $query_parameters = []
+    ): object
+    {
+        $url = $this->baseUrl . $url;
 
         $response = $this->header->delete($url, $query_parameters);
+
+        $response_status = $response->status();
+
+        if ($response_status === 403 || $response_status === 429) {
+            $this->waitThrottle($response_status);
+
+            $this->destroy($url, $query_parameters);
+        }
 
         return $this->parseResponse($response);
     }
@@ -31,7 +49,8 @@ class Api extends LaravelFattureInCloudV2
     protected function data(
         array $data,
         array $fields
-    ): array {
+    ): array
+    {
         $parsed_data = [];
         foreach ($data as $key => $value) {
             if (in_array($key, $fields)) {
@@ -42,11 +61,29 @@ class Api extends LaravelFattureInCloudV2
         return $parsed_data;
     }
 
+    private function waitThrottle(
+        int $status
+    )
+    {
+        switch ($status) {
+            case 403:
+                usleep(config('fatture-in-cloud-v2.limits.403'));
+                break;
+            case 429:
+                usleep(config('fatture-in-cloud-v2.limits.429'));
+                break;
+            default:
+                usleep(config('fatture-in-cloud-v2.limits.default'));
+                break;
+        }
+    }
+
     private function parseResponse($response): object
     {
-        return (object) [
+        return (object)[
             'success' => $response->status() === 200,
-            'data'    => json_decode($response),
+            'data' => json_decode($response),
         ];
     }
 }
+
