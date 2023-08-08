12633c3543d18fbf14aaa8fd57d21cf39d839b1d
Riccardo Arena <riccardo@puntoquindici.it>
2023-08-08 09:38:14 +0000

    Add Info/PaymentAccounts list + tests

--- tests/Fake/InfoFakeResponse.php
+++ tests/Fake/InfoFakeResponse.php
@@ -2,6 +2,7 @@
 
 namespace OfflineAgency\LaravelFattureInCloudV2\Tests\Fake;
 
+use OfflineAgency\LaravelFattureInCloudV2\Tests\Fake\Info\SinglePaymentAccount;
 use OfflineAgency\LaravelFattureInCloudV2\Tests\Fake\Info\SingleVat;
 
 class InfoFakeResponse extends FakeResponse
@@ -29,6 +30,32 @@ class InfoFakeResponse extends FakeResponse
     }
 
     public function getVatTypesFakeError(
+        array $params = []
+    ) {
+        return json_encode((new ErrorFakeResponse())->getErrorFake($params));
+    }
+
+    public function getPaymentAccountsFakeList(
+        array $params = []
+    ) {
+        return json_encode([
+            'data' => [
+                (new SinglePaymentAccount())->getPaymentAccountFakeDetail($params)
+            ]
+        ]);
+    }
+
+    public function getEmptyPaymentAccountsFakeList(
+        array $params = []
+    ) {
+        return json_encode(array_merge(
+            [
+                'data' => [],
+            ]
+        ));
+    }
+
+    public function getPaymentAccountsFakeError(
         array $params = []
     ) {
         return json_encode((new ErrorFakeResponse())->getErrorFake($params));
--- tests/Feature/InfoEntityTest.php
+++ tests/Feature/InfoEntityTest.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 use OfflineAgency\LaravelFattureInCloudV2\Api\Info;
 use OfflineAgency\LaravelFattureInCloudV2\Entities\Error;
 use OfflineAgency\LaravelFattureInCloudV2\Entities\Info\InfoList;
+use OfflineAgency\LaravelFattureInCloudV2\Entities\Info\PaymentAccount as PaymentAccountEntity;
 use OfflineAgency\LaravelFattureInCloudV2\Entities\Info\Vat as VatEntity;
 use OfflineAgency\LaravelFattureInCloudV2\Tests\Fake\InfoFakeResponse;
 use OfflineAgency\LaravelFattureInCloudV2\Tests\TestCase;
@@ -71,6 +72,67 @@ class InfoEntityTest extends TestCase
 
         $info = new Info();
         $response = $info->listVatTypes();
+
+        $this->assertInstanceOf(Error::class, $response);
+    }
+
+    public function test_list_payment_accounts()
+    {
+        Http::fake([
+            'info/payment_accounts' => Http::response(
+                (new InfoFakeResponse())->getPaymentAccountsFakeList()
+            ),
+        ]);
+
+        $info = new Info();
+        $response = $info->listPaymentAccounts();
+
+        $this->assertNotNull($response);
+        $this->assertInstanceOf(InfoList::class, $response);
+        $this->assertIsArray($response->getItems());
+        $this->assertCount(1, $response->getItems());
+        $this->assertInstanceOf(PaymentAccountEntity::class, $response->getItems()[0]);
+    }
+
+    public function test_list_payment_accounts_has_payment_accounts_method()
+    {
+        Http::fake([
+            'info/payment_accounts' => Http::response(
+                (new InfoFakeResponse())->getPaymentAccountsFakeList()
+            ),
+        ]);
+
+        $info = new Info();
+        $response = $info->listPaymentAccounts();
+
+        $this->assertTrue($response->hasItems());
+    }
+
+    public function test_empty_list_payment_accounts_has_payment_accounts_method()
+    {
+        Http::fake([
+            'info/payment_accounts' => Http::response(
+                (new InfoFakeResponse())->getEmptyPaymentAccountsFakeList()
+            ),
+        ]);
+
+        $info = new Info();
+        $response = $info->listPaymentAccounts();
+
+        $this->assertFalse($response->hasItems());
+    }
+
+    public function test_error_on_list_payment_accounts()
+    {
+        Http::fake([
+            'info/payment_accounts' => Http::response(
+                (new InfoFakeResponse())->getPaymentAccountsFakeError(),
+                401
+            ),
+        ]);
+
+        $info = new Info();
+        $response = $info->listPaymentAccounts();
 
         $this->assertInstanceOf(Error::class, $response);
     }
