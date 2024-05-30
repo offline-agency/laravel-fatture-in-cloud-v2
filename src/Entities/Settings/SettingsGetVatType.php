7a9a5305fe241b816f7fd74c071ba84423412549
nsnever-admin <vecchiatoriccardo0606@gmail.com>
2024-05-30 15:13:32 +0200

    first commit: create entities for received document

---
+++ src/Entities/Settings/SettingsGetVatType.php
@@ -0,0 +1,12 @@
+<?php
+namespace OfflineAgency\LaravelFattureInCloudV2\Entities\Settings;
+
+use OfflineAgency\LaravelFattureInCloudV2\Entities\AbstractEntity;
+
+class SettingsGetVatType extends AbstractEntity{
+
+    /**
+     * @var bool
+     * */
+    public $editable;
+}
---
+++ src/Entities/Taxes/TaxesCreate.php
@@ -0,0 +1,12 @@
+<?php
+namespace OfflineAgency\LaravelFattureInCloudV2\Entities\Taxes;
+
+use OfflineAgency\LaravelFattureInCloudV2\Entities\AbstractEntity;
+
+class TaxesCreate extends AbstractEntity
+{
+    /**
+     * @var string
+     */
+    public $attachment_token;
+}
